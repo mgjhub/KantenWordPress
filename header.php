2bcb1aff7450b62a3c7d29391f1ebfa9bed6629a
Tanya <tanhan01@easv365.dk>
2023-05-23 11:27:11 +0200

    css

--- header.php
+++ header.php
@@ -11,5 +11,6 @@ This is a template that displays all of the <head> section and up until <main>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <?php wp_head( ); ?>
+    <link rel="stylesheet" href="https://use.typekit.net/xfs3fuf.css" >
 </head>
 <body <?php body_class( ); ?>>
